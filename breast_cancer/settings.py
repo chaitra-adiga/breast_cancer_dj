0c93708c5364ded3e88f2f6fc82d435c9aaeff7c
Chaitra Adiga <itsca03@gmail.com>
2024-10-12 09:06:17 +0530

    google login & registration form

--- breast_cancer/settings.py
+++ breast_cancer/settings.py
@@ -30,6 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
+SITE_ID=2
+
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -38,7 +42,24 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "website",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
 ]
+
+
+SOCIALACCOUNT_PROVIDERS={
+    "google":{
+        "SCOPE":[
+            "profile",
+            "email"
+        ],
+        "AUTH_PARAMS":{"access_type":"online"}
+    }
+}
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -48,6 +69,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware", #added by copilot
 ]
 
 ROOT_URLCONF = "breast_cancer.urls"
@@ -126,3 +148,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTHETICATIONS_BACKENDS ={
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend"
+}
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
--- website/views.py
+++ website/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 
 
-def home(request):  # we can't call it login because it wiil conflict with built-in function
+def home(request):  # we can't call it login because it will conflict with built-in function
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -22,10 +23,27 @@ def home(request):  # we can't call it login because it wiil conflict with built
     
 
 def register_user(request):
-     return render(request,'register.html',{})
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
 
+	return render(request, 'register.html', {'form':form})
 def logout_user(request):
 	logout(request)
 	messages.success(request, "You Have Been Logged Out...")
 	return redirect('home')
 
+def logout_view(request):
+      logout(request)
+      return redirect("/")
